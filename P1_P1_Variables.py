64f59628886a2f46114d72618be219b0c2de317c
AlexaGutieMora <143145315+AlexaGutieMora@users.noreply.github.com>
2025-03-14 17:38:26 -0600

    Add files via upload

---
+++ P1_P1_Variables.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 25 23:14:30 2025
+
+@author: k
+"""
+
+################## CAPÍTULO: VARIABLES
+text = "Hola, este es un ejemplo de variable" 
+print(text) #imprime la variable
+
+numero_1 = 1 #guarda números
+numero_2 = 7
+sumatoria = numero_1 + numero_2 #crea una variable que guarde la suma de los números
+print(sumatoria)
+
+vartext_1 = 'Ejemplo comillas simples' #Ejemplo de cómo se pueden guardar las variables de texto
+vartext_2 = "Ejemplo comillas dobles"
+
+#Ejemplos de cómo unir strings
+concatenar = "Soy" + " Alexa."
+print(concatenar)
+
+string_1 = "Soy"
+string_2 = " Alexa."
+cadena = string_1 + string_2
+print(cadena)
+
+palabra_1 = "Soy"
+print(palabra_1 + " Alexa.")
+
+nombre = "Alexa"
+apellido_1 = "Gutierrez"
+apellido_2 = "Morales"
+nombre_completo  = nombre + " " + apellido_1 + " " + apellido_2 
+print(nombre_completo)
+
+#
+titulo = "alexa gutierrez morales".title() #Pone en mayúsuclas la primera letra de cada palabra
+print(titulo)
+
+minusculas = "ALeXa GuTIERReZ MoRAlES".lower() #pone todas las letras en minúsculas
+print(minusculas)
+
+mayusculas = "ALeXa GuTIERReZ MoRAlES".upper() #Pone todas las letras en mayúsculas
+print(mayusculas)
+
+print("-Python.\n-JavaScript.\n-Java.\n-PHP.\n-TypeScript.\n-SQL.\n-COBOL.") #Ejemplo del funcionamiento de "\n" para saltos de línea
+
+    
+
+
+
+
+
